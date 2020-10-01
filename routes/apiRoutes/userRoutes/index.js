572c3b84b4aed5615db1110652afbb1b5fd439d6
Lindsey Smith <linds@lindseys-mbp.lan>
2020-09-30 22:25:12 -0600

    cleaning up code

--- routes/apiRoutes/userRoutes/index.js
+++ routes/apiRoutes/userRoutes/index.js
@@ -1,19 +1,21 @@
 const router = require('express')
   .Router();
 const connection = require('../../../config/connection');
+const {insertUser} = require('../../../model/userQueries');
+const { fetchUsers } = require('../../../model/userOrm');
 // /api/users prepended to every Route
 router.route('/')
   .get(async (_req, res) => {
-    const query = 'SELECT * FROM users;';
-    const [rows, fields] = await connection.query(query);
-    console.log('I AM ROWS', rows);
-    console.log('I AM FIELDS', fields);
-    res.json(rows);
+    try {
+      const users = await fetchUsers();
+      res.json(users);
+    } catch (error) {
+      res.status(400).json(e);
+    }
   })
   .post(async (req, res) => {
     const userInput = req.body;
-    const query = 'INSERT INTO users SET ?;';
-    const result = await connection.query(query, userInput);
+    const result = await connection.query(insertUser, userInput);
     res.json(result);
   });
 module.exports = router;
